e3351607f0536826ce9b548d2fe7d106b039aba2
scott <scottshiro@outlook.com>
2026-05-14 02:11:09 -0400

    revert app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,3 @@
-// import { Grid, Box } from "@chakra-ui/react";
-// import Sidebar from "./components/Sidebar";
-// import About from "./components/About";
-// import Experiences from "./components/Experiences";
-// import Projects from "./components/Projects";
-// import CaseStudies from "./components/CaseStudies";
-
-// const App = () => {
-//   return (
-//     <Grid
-//       templateColumns={["1fr", "300px 1fr"]}
-//       gap={10}
-//       maxW="1200px"
-//       mx="auto"
-//       p={10}
-//     >
-//       <Box position="sticky" top="4rem" h="fit-content">
-//         <Sidebar />
-//       </Box>
-//       <Box display="flex" flexDirection="column" gap={12}>
-//         <About />
-//         <Experiences />
-//         <Projects />
-//         <CaseStudies />
-//       </Box>
-//     </Grid>
-//   );
-// };
-
-// export default App;
-
 import { Grid, Box } from "@chakra-ui/react";
 import Sidebar from "./components/Sidebar";
 import About from "./components/About";
@@ -39,18 +8,16 @@ import CaseStudies from "./components/CaseStudies";
 const App = () => {
   return (
     <Grid
-      templateColumns={{ base: "1fr", md: "300px 1fr" }}
-      gap={{ base: 6, md: 10 }}
+      templateColumns={["1fr", "300px 1fr"]}
+      gap={10}
       maxW="1200px"
       mx="auto"
-      px={{ base: 4, md: 10 }}
-      py={{ base: 6, md: 10 }}
+      p={10}
     >
-      <Box position={{ base: "static", md: "sticky" }} top="4rem" h="fit-content">
+      <Box position="sticky" top="4rem" h="fit-content">
         <Sidebar />
       </Box>
-
-      <Box display="flex" flexDirection="column" gap={{ base: 10, md: 12 }}>
+      <Box display="flex" flexDirection="column" gap={12}>
         <About />
         <Experiences />
         <Projects />
