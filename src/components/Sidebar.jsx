9e45bd30381a5a958195493769b749aadb7af9e6
scott <scottshiro@outlook.com>
2026-05-17 20:36:38 -0400

    wrap vstack links inside a nav

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,36 +22,38 @@ const Sidebar = () => {
       </Text>
 
       {/* Navigation */}
-      <VStack align="start" spacing={2} mt={6}>
-        {[
-          { label: "ABOUT", href: "#" },
-          { label: "EXPERIENCE", href: "#experience" },
-          { label: "PROJECTS", href: "#projects" },
-          { label: "CASE STUDIES", href: "#case-studies" },
-        ].map((item) => (
-          <Link
-            key={item.href}
-            href={item.href}
-            color="accent"
-            position="relative"
-            _after={{
-              content: '""',
-              position: "absolute",
-              bottom: "-2px",
-              left: "0",
-              width: "0%",
-              height: "1px",
-              backgroundColor: "highlight",
-              transition: "width 0.25s ease",
-            }}
-            _hover={{
-              _after: { width: "100%" },
-            }}
-          >
-            {item.label}
-          </Link>
-        ))}
-      </VStack>
+      <nav>
+        <VStack align="start" spacing={2} mt={6}>
+          {[
+            { label: "ABOUT", href: "#" },
+            { label: "EXPERIENCE", href: "#experience" },
+            { label: "PROJECTS", href: "#projects" },
+            { label: "CASE STUDIES", href: "#case-studies" },
+          ].map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              color="accent"
+              position="relative"
+              _after={{
+                content: '""',
+                position: "absolute",
+                bottom: "-2px",
+                left: "0",
+                width: "0%",
+                height: "1px",
+                backgroundColor: "highlight",
+                transition: "width 0.25s ease",
+              }}
+              _hover={{
+                _after: { width: "100%" },
+              }}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </VStack>
+      </nav>
 
       {/* Contact Links */}
       <Box mt={10}>
